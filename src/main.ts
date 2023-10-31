f7f8db5c405b5da230bbf5bf0a5e9298e7459bb2
Svetlin Ralchev <iamralch@users.noreply.github.com>
2023-10-31 14:29:05 +0400

    Initial version
    Signed-off-by: Svetlin Ralchev <iamralch@users.noreply.github.com>

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,29 @@
 import * as core from '@actions/core'
-import { wait } from './wait'
+import * as github from '@actions/github'
 
-/**
- * The main function for the action.
- * @returns {Promise<void>} Resolves when the action is complete.
- */
-export async function run(): Promise<void> {
-  try {
-    const ms: string = core.getInput('milliseconds')
+async function run() {
+  const token = core.getInput('github-token')
+  const client = github.getOctokit(token)
+  // find the latest release
+  const { data: releases } = await client.rest.repos.listReleases({
+    owner: github.context.payload.repository!.owner.login,
+    repo: github.context.payload.repository!.name,
+    per_page: 1,
+    page: 1
+  })
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
-
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
-  } catch (error) {
-    // Fail the workflow run if an error occurs
-    if (error instanceof Error) core.setFailed(error.message)
+  // extract the version from the tag
+  const version =
+    releases.length > 0 ? releases[0].tag_name.replace(/^(v)/, '') : '0.0.0'
+  // export the version
+  core.exportVariable('GH_REPOSITORY_RELEASE_VERSION', version)
+  // if the event is a pull request
+  if (github.context.eventName == 'pull_request') {
+    const pr_number = github.context.payload.pull_request!.number
+    const pr_version = `${version}-pr.${pr_number}`
+    // export the version
+    core.exportVariable('GH_REPOSITORY_PULL_REQUEST_VERSION', pr_version)
   }
 }
+
+run()
